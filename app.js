0126addbbfb8e5733f8e8304603c6aa4c9158ad7
Nishal Najeeb <nishal.najeeb@students.mq.edu>
2021-01-09 17:01:36 +0400

    Set up workspace

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash'); 
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const mongoSanitize = require('express-mongo-sanitize');
+
+const dbUrl = 'mongodb://localhost:27017/chitter';
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('useFindAndModify', false);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+});
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true}));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(mongoSanitize());
+
+const secret = 'thishsouldbeanactualsecret';
+
+const sessionConfig = {
+    store,
+    name: 'session',
+    secret,
+    resave: false,
+    //secure: true
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true, //this is an extra security thing
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    }
+}
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
