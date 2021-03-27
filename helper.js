d19ba8913d37ce1ae53e5518f76b64810d31d586
Nishal1 <nishal.najeeb@students.mq.edu.au>
2021-03-27 17:20:33 +0400

    add basic mutual follow system

--- helper.js
+++ helper.js
@@ -43,3 +43,22 @@ module.exports.hasAldreadyLiked = (post, signedInUserId) => {
     }
     return false;
 };
+
+module.exports.majorityFollowing = (l1, l2) => {
+    let list = l1.concat(l2);
+    let c = 0;
+    let count = 0;
+    for(let i = 0; i < list.length; i++){
+        if(count == 0){
+            c = list[i];
+            count++;
+        }
+        else if(list[i].equals(c)) {
+            count++;
+        }
+        else {
+            count--;
+        }
+    }
+    return c;
+}
