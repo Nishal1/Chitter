85bb2d8a4a59bf5ae958ce1b212099e6f704ff59
Praghadiesh <praghadieshnikhil234@gmail.com>
2021-03-30 13:20:14 +0530

    add homepage scripts

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -5,8 +5,8 @@ const bar2 = document.querySelector('#bar2');
 const bar3 = document.querySelector('#bar3');
 const bar4 = document.querySelector('#bar4');
 
-document.addEventListener("scroll", function() {
-    if (isScrolledIntoView(showcase)){
+document.addEventListener('scroll', function () {
+    if (isScrolledIntoView(showcase)) {
         bar1.classList.add('active-bar');
         bar2.classList.remove('active-bar');
         bar3.classList.remove('active-bar');
@@ -17,21 +17,23 @@ document.addEventListener("scroll", function() {
         bar3.classList.remove('active-bar');
         bar4.classList.remove('active-bar');
     }
-}); 
+});
 
-var isScrolledIntoView = function(el){
-    var rect = el.getBoundingClientRect(), top = rect.top, height = rect.height, 
-      el = el.parentNode
+var isScrolledIntoView = function (el) {
+    var rect = el.getBoundingClientRect(),
+        top = rect.top,
+        height = rect.height,
+        el = el.parentNode;
     // Check if bottom of the element is off the page
-    if (rect.bottom < 0) return false
+    if (rect.bottom < 0) return false;
     // Check its within the document viewport
-    if (top > document.documentElement.clientHeight) return false
+    if (top > document.documentElement.clientHeight) return false;
     do {
-      rect = el.getBoundingClientRect()
-      if (top <= rect.bottom === false) return false
-      // Check if the element is out of view due to a container scrolling
-      if ((top + height) <= rect.top) return false
-      el = el.parentNode
-    } while (el != document.body)
+        rect = el.getBoundingClientRect();
+        if (top <= rect.bottom === false) return false;
+        // Check if the element is out of view due to a container scrolling
+        if (top + height <= rect.top) return false;
+        el = el.parentNode;
+    } while (el != document.body);
     return true;
-  }
+};
